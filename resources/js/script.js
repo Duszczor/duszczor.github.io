3176474c6cf0f6767c7409b7a35a1ecd45812455
dusio <duszczor@gmail.com>
2026-06-25 20:29:11 +0200

    fixes

--- resources/js/script.js
+++ resources/js/script.js
@@ -12,44 +12,6 @@ $(document).ready(function () {
     },
   );
 
-  /* Navigation scroll */
-  $(function () {
-    $('a[href*="#"]')
-      .not('[href="#"]')
-      .not('[href="#0"]')
-      .click(function (event) {
-        if (
-          location.pathname.replace(/^\//, "") ==
-            this.pathname.replace(/^\//, "") &&
-          location.hostname == this.hostname
-        ) {
-          var target = $(this.hash);
-          target = target.length
-            ? target
-            : $("[name=" + this.hash.slice(1) + "]");
-          if (target.length) {
-            event.preventDefault();
-            $("html, body").animate(
-              {
-                scrollTop: target.offset().top - 60,
-              },
-              1500,
-              function () {
-                var $target = $(target);
-                $target.focus();
-                if ($target.is(":focus")) {
-                  return false;
-                } else {
-                  $target.attr("tabindex", "-1");
-                  $target.focus();
-                }
-              },
-            );
-          }
-        }
-      });
-  });
-
   /* Animations on scroll */
   $(".js--wp-1").waypoint(
     function (direction) {
