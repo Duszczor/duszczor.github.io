32480849bf80e48abd055504cc64e6dff70abb98
dusio <duszczor@gmail.com>
2026-06-27 23:05:16 +0200

    fixes

--- resources/js/script.js
+++ resources/js/script.js
@@ -12,84 +12,10 @@ $(document).ready(function () {
     },
   );
 
-  /* Animations on scroll */
-  $(".js--wp-1").waypoint(
-    function (direction) {
-      $(".js--wp-1").addClass("animated fadeIn");
-    },
-    {
-      offset: "50%",
-    },
-  );
-
-  $(".js--wp-2").waypoint(
-    function (direction) {
-      $(".js--wp-2").addClass("animated fadeInUp");
-    },
-    {
-      offset: "50%",
-    },
-  );
-
-  $(".js--wp-3").waypoint(
-    function (direction) {
-      $(".js--wp-3").addClass("animated fadeIn");
-    },
-    {
-      offset: "50%",
-    },
-  );
-
-  $(".js--wp-4").waypoint(
-    function (direction) {
-      $(".js--wp-4").addClass("animated pulse");
-    },
-    {
-      offset: "50%",
-    },
-  );
-
-  /* Mobile nav */
-  $(".js--nav-icon").click(function () {
-    var nav = $(".js--main-nav");
-    var icon = $(".js--nav-icon i");
-    var navButton = $(".js--nav-icon");
-    var headerText = $(".hero-text-box");
-
-    nav.slideToggle(200);
-    if (icon.hasClass("ion-navicon-round")) {
-      icon.addClass("ion-close-round");
-      icon.removeClass("ion-navicon-round");
-      navButton.attr("aria-expanded", "true");
-      headerText.css("margin-top", "180px");
-    } else {
-      icon.removeClass("ion-close-round");
-      icon.addClass("ion-navicon-round");
-      navButton.attr("aria-expanded", "false");
-      headerText.css("margin-top", 0);
-    }
-  });
-
-  $(".js--main-nav").click(function () {
-    var nav = $(".js--main-nav");
-    var icon = $(".js--nav-icon i");
-    var navButton = $(".js--nav-icon");
-    var headerText = $(".hero-text-box");
-
-    if ($(".mobile-nav-icon").css("display") != "none") {
-      nav.slideToggle(200);
-      if (icon.hasClass("ion-navicon-round")) {
-        icon.addClass("ion-close-round");
-        icon.removeClass("ion-navicon-round");
-        navButton.attr("aria-expanded", "true");
-        headerText.css("margin-top", "180px");
-      } else {
-        icon.removeClass("ion-close-round");
-        icon.addClass("ion-navicon-round");
-        navButton.attr("aria-expanded", "false");
-        headerText.css("margin-top", 0);
-      }
-    }
+  document.querySelectorAll(".mobile-nav-details .main-nav a").forEach(function (link) {
+    link.addEventListener("click", function () {
+      document.querySelector(".mobile-nav-details").removeAttribute("open");
+    });
   });
 
   var prefersReducedMotion = window.matchMedia(
